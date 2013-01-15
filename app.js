6d2b7c34d2fd62fb08a397ec92c0647526e6b1fd
Song Zheng <coldangelfire311@gmail.com>
2013-01-14 17:56:29 -0800

    initial working file

---
+++ app.js
@@ -0,0 +1,46 @@
+// Required modules, Listed in the order of importance
+var OTSDK = require('opentok'); // OpenTok API for live video streaming
+var http = require('http');     // Listen to HTTP Requests
+var fs = require('fs');         // Read files
+var ejs = require('ejs');       // Templating Engine
+
+// OpenTok Constants for creating Session and Token values
+var OTKEY = '14971292';
+var OTSECRET = 'ecbe2b25afec7887bd72fe4763b87add8ce02658';
+
+// Setup when server first starts
+var urlSessions = {}; // mapping url to OpenTok Sessions
+// Initialize OpenTok Object
+var OpenTok = new OTSDK.OpenTokSDK(OTKEY, OTSECRET);
+// read view Template into a string
+var htmlString = fs.readFileSync('./view.ejs', 'utf8'); 
+
+// Write response after all data (session Id, token) is ready
+function sendResponse(str, responder, data){
+  responder.writeHead(200);
+  responder.end( ejs.render(str, data) );
+}
+
+// Start Server
+var server = http.createServer(function(req, res){
+  if(urlSessions[req.url]==undefined){ // No OpenTok sessionId for url
+    var clientIP = req.connection.remoteAddress;
+    OpenTok.createSession(clientIP, function(sessionId){
+      // sessionId received. Generate token and write response
+      var token = OpenTok.generateToken( {session_id: sessionId} );
+      var data = {OpenTokKey:OTKEY, sessionId: sessionId, token:token};
+      sendResponse( htmlString, res, data );
+      urlSessions[req.url] = sessionId;
+    });
+  }else{
+    // sessionId exists, use it to generate token and write response
+    sessionId = urlSessions[req.url];
+    var token = OpenTok.generateToken( {session_id: sessionId} );
+    var data = {OpenTokKey:OTKEY, sessionId: sessionId, token:token};
+    sendResponse( htmlString, res, data );
+  }
+
+  console.log( urlSessions );
+});
+
+server.listen(9393);
