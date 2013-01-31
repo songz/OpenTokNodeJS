0cb56bf8669453f0f245f23f2005c170609288a6
Song Zheng <coldangelfire311@gmail.com>
2013-01-31 07:46:46 -0800

    better variable names, refactor dry

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-// Required modules, Listed in the order of importance
-var OTSDK = require('opentok'); // OpenTok API for live video streaming
-var http = require('http');     // Listen to HTTP Requests
-var fs = require('fs');         // Read files
-var ejs = require('ejs');       // Templating Engine
+// ***
+// *** Required modules, Listed in the order of importance
+// ***
+var OpenTokLibrary = require('opentok');
+var HTTPServer = require('http');
+var StaticFileReader = require('fs');
+var TemplatingEngine = require('ejs');
 
-// OpenTok Constants for creating Session and Token values
+// ***
+// *** OpenTok Constants for creating Session and Token values
+// ***
 var OTKEY = process.env.TB_KEY;
 var OTSECRET = process.env.TB_SECRET;
 
-// Setup when server first starts
-var urlSessions = {}; // mapping url to OpenTok Sessions
-// Initialize OpenTok Object
-var OpenTok = new OTSDK.OpenTokSDK(OTKEY, OTSECRET);
-// read view Template into a string
-var htmlString = fs.readFileSync('./view.ejs', 'utf8'); 
+// ***
+// *** Setup when server first starts
+// ***
+var sessionForUrl = {};
+var OpenTokObject = new OpenTokLibrary.OpenTokSDK(OTKEY, OTSECRET);
+var viewTemplate = StaticFileReader.readFileSync('./view.ejs', 'utf8');
 
-// Start Server
-var server = http.createServer(function(req, res){
-  if(urlSessions[req.url]==undefined){ // No OpenTok sessionId for url
-    var clientIP = req.connection.remoteAddress;
-    OpenTok.createSession(clientIP, function(sessionId){
-      // sessionId received. Generate token and write response
-      var token = OpenTok.generateToken( {session_id: sessionId} );
-      var data = {OpenTokKey:OTKEY, sessionId: sessionId, token:token};
-      sendResponse( htmlString, res, data );
-      urlSessions[req.url] = sessionId;
+// ***
+// *** Start Server
+// ***
+var server = HTTPServer.createServer(function(req, res){
+
+  if(sessionForUrl[req.url]==undefined){ 
+    OpenTokObject.createSession(function(sessionId){
+      sessionForUrl[req.url] = sessionId;
+      sendResponse( res, sessionId );
     });
   }else{
-    // sessionId exists, use it to generate token and write response
-    sessionId = urlSessions[req.url];
-    var token = OpenTok.generateToken( {session_id: sessionId} );
-    var data = {OpenTokKey:OTKEY, sessionId: sessionId, token:token};
-    sendResponse( htmlString, res, data );
+    sessionId = sessionForUrl[req.url];
+    sendResponse( res, sessionId );
   }
-
-  console.log( urlSessions );
+  console.log( sessionForUrl );
 });
+
 server.listen(9393);
 
 // Write response after all data (session Id, token) is ready
-function sendResponse(str, responder, data){
+function sendResponse( responder, sessionId ){
+  // All session_ids need a corresponding token
+  var token = OpenTokObject.generateToken( {session_id: sessionId} );
+  var data = {OpenTokKey:OTKEY, sessionId: sessionId, token:token};
   responder.writeHead(200);
-  responder.end( ejs.render(str, data) );
+  responder.end( TemplatingEngine.render(viewTemplate, data) );
 }
 
