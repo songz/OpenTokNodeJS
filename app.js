8856d46cb00715704d5de3ffd0f5cb3c2d01cb0c
Song Zheng <coldangelfire311@gmail.com>
2013-03-28 13:37:53 -0700

    add ejs templating

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var OpenTokLibrary = require('opentok');
 var HTTPServer = require('http');
 var StaticFileReader = require('fs');
 var TemplatingEngine = require('ejs');
+var express = require('express');
 
 // ***
 // *** OpenTok Constants for creating Session and Token values
@@ -15,32 +16,52 @@ var OTSECRET = process.env.TB_SECRET;
 // ***
 // *** Setup when server first starts
 // ***
-var sessionForUrl = {};
+var urlSessions = {};
 var OpenTokObject = new OpenTokLibrary.OpenTokSDK(OTKEY, OTSECRET);
-var viewTemplate = StaticFileReader.readFileSync('./view.ejs', 'utf8');
+var viewTemplate = StaticFileReader.readFileSync('./views/index.ejs', 'utf8'); 
 
 // ***
-// *** Start Server
+// *** Setup Express to handle static files in public folder
+// *** Express is also great for handling url routing
 // ***
-var server = HTTPServer.createServer(function(req, res){
+var app = express();
+app.use(express.static(__dirname + '/public'));
 
-  if(sessionForUrl[req.url]==undefined){ 
-    OpenTokObject.createSession(function(sessionId){
-      sessionForUrl[req.url] = sessionId;
-      sendResponse( res, sessionId );
-    });
-  }else{
-    sessionId = sessionForUrl[req.url];
-    sendResponse( res, sessionId );
-  }
-  console.log( sessionForUrl );
+// ***
+// *** When user goes to root directory, redirect them to a room (timestamp)
+// ***
+app.get("/", function( req, res ){
+  res.writeHead(302, { 'Location': Date.now() });
+  res.end();
 });
 
-server.listen(9393);
+// ***
+// *** When user goes to root directory, redirect them to a room (timestamp)
+// *** If sessionId does not exist for url, create one
+// ***
+app.get("/:room", function(req, res){
+  if(urlSessions[ req.params.room ] == undefined){
+    OpenTokObject.createSession(function(sessionId){
+      urlSessions[ req.params.room ] = sessionId;
+      sendResponse( sessionId, res );
+    });
+  }else{
+    sessionId = urlSessions[req.params.room];
+    sendResponse( sessionId, res );
+  }
+});
 
-// Write response after all data (session Id, token) is ready
-function sendResponse( responder, sessionId ){
-  // All session_ids need a corresponding token
+// ***
+// *** start server, listen to port (predefined or 9393)
+// ***
+var port = process.env.PORT || 9393;
+app.listen(port);
+
+// ***
+// *** All sessionIds need a corresponding token
+// *** generateToken and then sendResponse based on ejs template
+// ***
+function sendResponse( sessionId, responder ){
   var token = OpenTokObject.generateToken( {session_id: sessionId} );
   var data = {OpenTokKey:OTKEY, sessionId: sessionId, token:token};
   responder.writeHead(200);
